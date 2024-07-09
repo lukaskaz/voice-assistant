a4ac75238c16fc1b167c7b4ad9c8c7cda3e589bf
Lukasz Kazmierczak <lukask84@o2.pl>
2024-07-09 03:01:46 +0200

    Fixing CI errors v1
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- tests/cmake/dependencies.cmake
+++ tests/cmake/dependencies.cmake
@@ -1,8 +1,6 @@
 cmake_minimum_required(VERSION 3.10)
 
-find_package(Boost 1.74.0 REQUIRED COMPONENTS filesystem)
-# find_package(nlohmann_json 3.8.1 REQUIRED)
-# find_package(CURL REQUIRED)
+find_package(nlohmann_json 3.8.1 REQUIRED)
 
 include(ExternalProject)
 
